545cf947fa55a4366790ef5b7d947309a127f49c
ic3top <dvdvdv889@gmail.com>
2022-10-18 00:38:45 +0300

    Fix deps

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,5 @@ import { createServer } from './config/server';
 const app = createServer();
 
 app.listen(PORT || 3000, HOSTNAME as any, () => {
-	console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-});
-
-process.on('unhandledRejection', (error: Error) => {
-	logger.error('unhandledRejection', error.message);
+	logger.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
