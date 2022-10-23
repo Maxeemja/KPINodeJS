2f7d9a7dbefae3b87cae29505b71195e565ca69e
Maksym Hrytsiuk <maksym_hrytsiuk@epam.com>
2022-10-24 00:38:50 +0300

    minor refactoring, init log removed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { logger } from './utils/logger';
-import { PORT, HOSTNAME } from './config/preload';
+// import { logger } from './utils/logger';
+import { PORT } from './config/preload';
 import { createServer } from './config/server';
 
 const app = createServer();
 
-app.listen(PORT || 3000, HOSTNAME as any, () => {
-	logger.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-});
+app.listen(PORT || 3001);
