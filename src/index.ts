70c055413804f319095ba5a4f3551e86111d2e56
Maksym Hrytsiuk <Maksym_Hrytsiuk@epam.com>
2022-10-23 19:44:55 +0300

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { logger } from './utils/logger';
 import { PORT, HOSTNAME } from './config/preload';
 import { createServer } from './config/server';
-import http from "http";
 
-// const app = createServer();
-//
-// app.listen(PORT || 3000, HOSTNAME as any, () => {
-// 	logger.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-// });
+const app = createServer();
 
-http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
-}).listen(PORT)
+app.listen(PORT || 3000, HOSTNAME as any, () => {
+	logger.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
