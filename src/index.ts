06b0cbb3f74a367e5fd32f186fdd7eb38abbe281
Maksym Hrytsiuk <Maksym_Hrytsiuk@epam.com>
2022-10-23 18:47:35 +0300

    test commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { logger } from './utils/logger';
 import { PORT, HOSTNAME } from './config/preload';
 import { createServer } from './config/server';
+import http from "http";
 
-const app = createServer();
+// const app = createServer();
+//
+// app.listen(PORT || 3000, HOSTNAME as any, () => {
+// 	logger.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+// });
 
-app.listen(PORT || 3000, HOSTNAME as any, () => {
-	logger.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-});
+http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World');
+}).listen(PORT)
